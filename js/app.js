a1732fc51241818d95f8acd79494d6c4fdc01b76
Janus Nicon <janusnic@gmail.com>
2017-12-04 12:44:50 +0200

    added unit 5

--- js/app.js
+++ js/app.js
@@ -7,3 +7,50 @@ function toggle_panel(panel, background_layer) {
                 background_layer.classList.add('is-visible');
             }
      }
+
+  function ready() {
+      let buy = document.querySelector('.buy');
+      // console.log(buy);
+
+      buy.addEventListener('click', function () {
+        document.querySelector('.singleMember p').style.display = 'none';
+        document.querySelector('.singleMember .icon').style.display = 'none';
+        document.querySelector('.singleMember .buy').style.display = 'none';
+        document.querySelector('.singleMember .detail').style.display = 'block';
+        document.querySelector('.menu').style.top = '40%';
+        });
+
+      let cancel = document.querySelector('.cancel');
+
+      cancel.addEventListener('click', function () {
+        document.querySelector('.singleMember p').style.display = 'block';
+        document.querySelector('.singleMember .icon').style.display = 'block';
+        document.querySelector('.singleMember .buy').style.display = 'block';
+        document.querySelector('.singleMember .detail').style.display = 'none';
+        document.querySelector('.menu').style.top = '80%';
+        });
+
+      var addcart = document.querySelector('.addcart');
+        addcart.addEventListener('click', function (e) {
+        
+      var promise = new Promise(function(resolve, reject) {
+      
+        resolve(1);
+        var y = 180;
+        
+        document.querySelector('.contentItem').style.transform = 'rotateY(' + y + 'deg)';
+        });
+
+        promise.then(function(val) {
+          setTimeout(function() {
+            document.querySelector('.contentItem').style.transform = 'rotateY(0deg)';
+            document.querySelector('.singleMember p').style.display = 'block';
+            document.querySelector('.singleMember .icon').style.display = 'block';
+            document.querySelector('.singleMember .buy').style.display = 'block';
+            document.querySelector('.singleMember .detail').style.display = 'none';
+            document.querySelector('.menu').style.top = '80%';
+          }, 2000);
+        }).then(function(val) {
+        });
+      });
+    }
