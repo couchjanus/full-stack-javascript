70fe6f40f1738559fa4b04ad022b7f5d7f840a85
Janus Nicon <janusnic@gmail.com>
2017-12-11 15:53:44 +0200

    added unit 8

--- js/app.js
+++ js/app.js
@@ -1,56 +1,20 @@
+jQuery.extend( jQuery.easing, {
+  
+      easeInOutExpo: function (x, t, b, c, d) {
+            if (t==0) return b;
+            if (t==d) return b+c;
+            if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+        },
+  });
+
 function toggle_panel(panel, background_layer) {
-            if (panel.classList.contains('speed-in')) {
-                panel.classList.remove('speed-in');
-                background_layer.classList.remove('is-visible');
-            } else {
-                panel.classList.add('speed-in');
-                background_layer.classList.add('is-visible');
-            }
-     }
 
-  function ready() {
-      let buy = document.querySelector('.buy');
-      // console.log(buy);
-
-      buy.addEventListener('click', function () {
-        document.querySelector('.singleMember p').style.display = 'none';
-        document.querySelector('.singleMember .icon').style.display = 'none';
-        document.querySelector('.singleMember .buy').style.display = 'none';
-        document.querySelector('.singleMember .detail').style.display = 'block';
-        document.querySelector('.menu').style.top = '40%';
-        });
-
-      let cancel = document.querySelector('.cancel');
-
-      cancel.addEventListener('click', function () {
-        document.querySelector('.singleMember p').style.display = 'block';
-        document.querySelector('.singleMember .icon').style.display = 'block';
-        document.querySelector('.singleMember .buy').style.display = 'block';
-        document.querySelector('.singleMember .detail').style.display = 'none';
-        document.querySelector('.menu').style.top = '80%';
-        });
-
-      var addcart = document.querySelector('.addcart');
-        addcart.addEventListener('click', function (e) {
-        
-      var promise = new Promise(function(resolve, reject) {
-      
-        resolve(1);
-        var y = 180;
-        
-        document.querySelector('.contentItem').style.transform = 'rotateY(' + y + 'deg)';
-        });
-
-        promise.then(function(val) {
-          setTimeout(function() {
-            document.querySelector('.contentItem').style.transform = 'rotateY(0deg)';
-            document.querySelector('.singleMember p').style.display = 'block';
-            document.querySelector('.singleMember .icon').style.display = 'block';
-            document.querySelector('.singleMember .buy').style.display = 'block';
-            document.querySelector('.singleMember .detail').style.display = 'none';
-            document.querySelector('.menu').style.top = '80%';
-          }, 2000);
-        }).then(function(val) {
-        });
-      });
+         if (panel.hasClass('speed-in')) {
+             panel.removeClass('speed-in');
+             background_layer.removeClass('is-visible');
+         } else {
+                panel.addClass('speed-in');
+                background_layer.addClass('is-visible');
+                }
     }
